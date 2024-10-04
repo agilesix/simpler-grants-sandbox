b46c6c127959a47f1c001fa46f81a46eccf87d3e
David Dudas <david.dudas@intuitial.com>
2024-10-04 15:48:12 -0700

    add fact tables to db schema

--- delivery-metrics/sql/create_delivery_metrics_db.sql
+++ delivery-metrics/sql/create_delivery_metrics_db.sql
@@ -1,26 +1,21 @@
 # drop tables
 
-DROP TABLE IF EXISTS issue;
-DROP TABLE IF EXISTS epic;
-DROP TABLE IF EXISTS sprint;
 DROP TABLE IF EXISTS deliverable;
+DROP TABLE IF EXISTS epic;
+DROP TABLE IF EXISTS issue;
+DROP TABLE IF EXISTS issue_history;
+DROP TABLE IF EXISTS issue_sprint_map;
+DROP TABLE IF EXISTS sprint;
 DROP TABLE IF EXISTS quad;
 
 # create tables
 
-CREATE TABLE issue (
+CREATE TABLE deliverable (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	guid TEXT UNIQUE NOT NULL,
 	title TEXT NOT NULL,
-	type TEXT NOT NULL,
-	points INTEGER NOT NULL DEFAULT 0,
-	status TEXT,
-	opened_date DATE,
-	closed_date DATE,
-	is_closed INTEGER,
-	parent_issue_guid TEXT,
-	epic_id TEXT,
-	sprint_id TEXT,
+	pillar TEXT, 
+	quad_id INTEGER,
 	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	t_modified TIMESTAMP 
 );
@@ -29,8 +24,40 @@ CREATE TABLE epic (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	guid TEXT UNIQUE NOT NULL,
 	title TEXT NOT NULL,
+	deliverable_id INTEGER,
 	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	t_modified TIMESTAMP 
+);
+
+CREATE TABLE issue (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	guid TEXT UNIQUE NOT NULL,
+	title TEXT NOT NULL,
+	type TEXT NOT NULL,
+	points INTEGER NOT NULL DEFAULT 0,
+	opened_date DATE,
+	closed_date DATE,
+	parent_issue_guid TEXT,
+	epic_id INTEGER,
+	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	t_modified TIMESTAMP 
+);
+
+CREATE TABLE issue_history (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	issue_id INTEGER NOT NULL,
+	status TEXT NOT NULL,
+	is_closed INTEGER NOT NULL,
+	effective DATE NOT NULL,
+	t_modified TIMESTAMP,
+	UNIQUE(issue_id, effective)
+);
+
+CREATE TABLE issue_sprint_map (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	issue_id INTEGER NOT NULL,
+	sprint_id TEXT NOT NULL,
+	effective DATE NOT NULL
 );
 
 CREATE TABLE sprint (
@@ -40,19 +67,10 @@ CREATE TABLE sprint (
 	start_date DATE,
 	end_date DATE,
 	duration INTEGER,
+	quad_id INTEGER,
 	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	t_modified TIMESTAMP 
 );
-
-CREATE TABLE deliverable (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	guid TEXT UNIQUE NOT NULL,
-	title TEXT NOT NULL,
-	pillar TEXT, 
-	t_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	t_modified TIMESTAMP 
-);
-
 
 CREATE TABLE quad (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
