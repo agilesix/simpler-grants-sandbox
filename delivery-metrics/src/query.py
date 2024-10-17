4c20fdafce1eb0954233ef6fdd02eab5b6463180
David Dudas <david.dudas@intuitial.com>
2024-10-16 19:52:35 -0700

    refactored for readability; rewrote sql statements for correctness

--- delivery-metrics/src/query.py
+++ delivery-metrics/src/query.py
@@ -3,167 +3,253 @@
 import sys
 sys.path.insert(0, './loader')
 
+from argparse import ArgumentParser
 from delivery_metrics_config import DeliveryMetricsConfig
 from delivery_metrics_database import DeliveryMetricsDatabase
 import functools 
 import time
 
 
-class DeliveryMetricsCalculator:
+class DeliveryMetricsPercentComplete:
 
-	def __init__(self, dbh):
-		self.dbh = dbh
+	def __init__(self, config):
+		self.config = config
+		self.dbh = DeliveryMetricsDatabase(config)
+		self.max_effective_date = self.dbh.getEffectiveDate()
+		self.verbose = False
 
 
-	def getPercentComplete(self):
+	def calculate(self):
 
-		deliverable_sql = ''' 
-			select 
-				d.id, 
-				d.title, 
-				m.d_effective,
-				q.name
-			from 
-				deliverable d 
-			inner join deliverable_quad_map m on m.deliverable_id = d.id
-			inner join quad q on q.id = m.quad_id
-			where
-				m.d_effective <= ?
-			order by d_effective desc;
-		'''
-
-		epic_sql = '''
-			select 
-				e.id, 
-				e.title, 
-				m.d_effective
-			from 
-				epic e
-			inner join epic_deliverable_map m on m.epic_id = e.id
-			where 
-				m.deliverable_id = ? and
-				m.d_effective <= ?
-			order by d_effective desc
-		'''
-
-		issue_sql = '''
-			select 
-				i.id, 
-				i.title, 
-				h.points, 
-				h.status, 
-				h.is_closed,
-				h.d_effective 
-			from 
-				issue i 
-			inner join issue_history h on h.issue_id = i.id
-			where 
-				i.epic_id = ? and
-				h.d_effective <= ?
-			order by d_effective 
-		'''
-
-		# init vars
-		quad_is_displayed = dict()
-		max_effective_date = self.dbh.getEffectiveDate()
-		unique_issues = dict()
-
-		# get deliverables in each quad
+		# initialize cursor
 		cursor = self.dbh.cursor()
-		cursor.execute(deliverable_sql, (max_effective_date,))
-		deliverables = cursor.fetchall()
-		for d_row in deliverables: 
 
-			# parse results
-			d_id = d_row[0]
-			d_title = d_row[1]
-			d_effective = d_row[2]
-			quad_name = d_row[3]
-			
+		# iterate quads
+		quads = self.getQuads(cursor)
+		for quad_id, quad_name in quads.items():
+
 			# output
-			if quad_is_displayed.get(quad_name) is None:
-				print("QUAD: {}".format(quad_name))
-				quad_is_displayed[quad_name] = True
-			print("\tDELIVERABLE: {} :: {}".format(d_title, d_effective ))
-			
-			# get epics in each deliverable
-			cursor.execute(epic_sql, (d_id, max_effective_date))
-			epics = cursor.fetchall()
-			for e_row in epics:
+			print("[QUAD] {}".format(quad_name))
+
+			# iterate deliverables
+			deliverables = self.getDeliverables(cursor, quad_id)
+			for deliverable_id, d in deliverables.items():
 
 				# output
-				e_id = e_row[0]
-				e_title = e_row[1]
-				e_effective = e_row[2]
-				print("\t\tEPIC: {} :: {}".format(e_title, e_effective ))
+				print("\t[DELIVERABLE] {} (effective {})".format(d.get('title'), d.get('effective')))
+
+				# initialize counters
+				total = DeliveryMetricsPercentCompleteTotals()
+			
+				# iterate epics
+				epics = self.getEpics(cursor, deliverable_id)
+				for epic_id, e in epics.items():
 				
-				# get issues in each epic
-				cursor.execute(issue_sql, (e_id, max_effective_date))
-				issues = cursor.fetchall()
-				for i_row in issues:
-
 					# output
-					i_id = i_row[0]
-					i_title = i_row[1]
-					i_points = i_row[2]
-					i_status = i_row[3]
-					i_closed = bool(i_row[4])
-					i_effective = i_row[5]
-					#print("\t\t\tISSUE: {} :: {} :: {} :: {}".format(i_title, i_points, i_status, i_effective))
+					if self.verbose:
+						print("\t\t[EPIC] {} (effective {})".format(e.get('title'), e.get('effective') )) 
+				
+					# iterate issues
+					issues = self.getIssues(cursor, epic_id)
+					for issue_id, i in issues.items():
+					
+						# output
+						if self.verbose:
+							print("\t\t\t[ISSUE] {} (effective {}, points={}, closed={})".format(i.get('title'), i.get('effective'), i.get('points'), i.get('closed')))
 
-					# aggregate results for processing outside these nested loops
-					if i_id not in unique_issues:
-						unique_issues[i_id] = {
-							'is_closed': False,
-							'points': 0
-						}
-					unique_issues[i_id]['is_closed'] = i_closed
-					unique_issues[i_id]['points'] = i_points
+						# increment counters
+						total.issues += 1
+						total.points += i.get('points', 0)
+						if i.get('closed', False):
+							total.issues_closed +=1
+							total.points_closed += i.get('points', 0)
 
+					# end issue iteration loop
+
+				# end epic iteration loop
+
+				# calculate and output results
+				total.printResults()
+
+			# end deliverable iteration loop
+			
+		# end quad iteration loop
+
+		# close cursor
 		cursor.close()
 
-		# calculate % complete (issues)
-		total_issues = len(unique_issues)
-		total_issues_closed = functools.reduce(
-			lambda x, y: x+1 if y['is_closed'] is True else x,
-			unique_issues.values(),
-			0
-		)
-		percent_complete_issues = round(100*(total_issues_closed / total_issues), 1)
 
-		# calculate % complete (points)
-		total_points = functools.reduce(lambda x, y: x+int(y['points']), unique_issues.values(), 0)
-		total_points_closed = functools.reduce(
-			lambda x, y: x+y['points'] if y['is_closed'] is True else x,
-			unique_issues.values(),
-			0
-		)
-		percent_complete_points = round(100*(total_points_closed / total_points), 1)
+	def getQuads(self, cursor):
 
-		# output summarized results
-		print("RESULTS")
-		print("Total Issues Closed: {}".format(str(total_issues_closed)))
-		print("Total Issues: {}".format(str(total_issues)))
-		print("Issues Complete: {}%".format(str(percent_complete_issues)))
-		print("Total Points Closed: {}".format(str(total_points_closed)))
-		print("Total Points: {}".format(str(total_points)))
-		print("Points Complete: {}%".format(str(percent_complete_points)))
-		print("---")
+		# init data store
+		quads = dict()
+
+		# define sql
+		sql = '''
+			select
+				id,
+				name
+			from 
+				quad
+			where
+				start_date <= ?
+		'''
+
+		# get quads
+		cursor.execute(sql, (self.max_effective_date,))
+		all_rows = cursor.fetchall()
+		for row in all_rows:
+			q_id = row[0]
+			quads[q_id] = row[1]
+
+		return quads
 
 
+	def getDeliverables(self, cursor, quad_id):
+
+		# init data store
+		deliverables = dict()
+
+		# define sql
+		sql = ''' 
+			select 
+				deliverable_id,
+				max(d_effective),
+				d.title 
+			from 
+				deliverable_quad_map m 
+			inner join deliverable d on d.id = m.deliverable_id
+			where
+				quad_id = ? and
+				d_effective <= ?
+			group by deliverable_id
+		'''
+
+		# get deliverables
+		cursor.execute(sql, (quad_id, self.max_effective_date))
+		all_rows = cursor.fetchall()
+		for row in all_rows: 
+			d_id = row[0]
+			deliverables[d_id] = {
+				'effective': row[1],
+				'title': row[2]
+			}
+
+		return deliverables
+	
+
+	def getEpics(self, cursor, deliverable_id):
+
+		# init data store
+		epics = dict()
+
+		# define sql
+		sql = '''
+			select 
+				epic_id,
+				max(d_effective),
+				e.title 
+			from 
+				epic_deliverable_map m
+			inner join epic e on e.id = m.epic_id 
+			where 
+				deliverable_id = ? and
+				d_effective <= ?
+			group by epic_id
+		'''
+
+		# get epics
+		cursor.execute(sql, (deliverable_id, self.max_effective_date))
+		all_rows = cursor.fetchall()
+		for row in all_rows: 
+			e_id = row[0]
+			epics[e_id] = {
+				'effective': row[1],
+				'title': row[2]
+			}
+
+		return epics
+
+
+	def getIssues(self, cursor, epic_id):
+
+		# init data store
+		issues = dict()
+
+		# define sql
+		sql = '''
+			select 
+				issue_id,
+				max(d_effective),
+				points,
+				is_closed,
+				i.title
+			from 
+				issue_history h 
+			inner join issue i on i.id = h.issue_id 
+			where 
+				epic_id = ? and
+				d_effective <= ?
+			group by issue_id
+		'''
+
+		# get issues
+		cursor.execute(sql, (epic_id, self.max_effective_date))
+		all_rows = cursor.fetchall()
+		for row in all_rows:
+			i_id = row[0]
+			issues[i_id] = {
+				'effective': row[1],
+				'points': row[2],
+				'closed': bool(row[3]),
+				'title': row[4]
+			}
+		
+		return issues
+
+
+class DeliveryMetricsPercentCompleteTotals:
+
+	def __init__(self):
+
+		self.issues = 0
+		self.issues_closed = 0
+		self.points = 0
+		self.points_closed = 0
+
+	
+	def printResults(self):
+
+		percent_complete_issues = round(100*(self.issues_closed / self.issues), 1)
+		percent_complete_points = round(100*(self.points_closed / self.points), 1)
+
+		print("\t\tTotal Issues: {}".format(str(self.issues)))
+		print("\t\tTotal Issues Closed: {}".format(str(self.issues_closed)))
+		print("\t\tIssues Complete: {}%".format(str(percent_complete_issues)))
+		print("\t\tTotal Points: {}".format(str(self.points)))
+		print("\t\tTotal Points Closed: {}".format(str(self.points_closed)))
+		print("\t\tPoints Complete: {}%".format(str(percent_complete_points)))
 
 
 if __name__ == "__main__":
 
-	#effective = time.strptime('20241007', '%Y%m%d')
-	effective = None
+	def parseDateArg(d):
+		return time.strptime(d, '%Y%m%d')
 
-	config = DeliveryMetricsConfig(effective)
-	dbh = DeliveryMetricsDatabase(config)
+	# define command line args
+	parser = ArgumentParser(description="Calculate % complete of deliverables in delivery metrics database")
+	parser.add_argument("-e", dest="yyyymmdd", type=parseDateArg, help="effective date to use in metrics calculation")
 
-	calc = DeliveryMetricsCalculator(dbh)
-	calc.getPercentComplete()
+	# get command line args
+	args = parser.parse_args()
 
-	dbh.disconnect()
+	# initialize config object
+	config = DeliveryMetricsConfig(args.yyyymmdd)
+	
+	# calculate metrics
+	print("...\ncalculating metrics with effective date <= {}".format(config.effectiveDate()))
+	metrics = DeliveryMetricsPercentComplete(config)
+	metrics.calculate()
+	print("metrics calculations are done!")
 
 
